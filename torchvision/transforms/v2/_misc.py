1b0d29569b79eb862f821c42344b233d6cf05c91
Justin Davis <judavis@nvidia.com>
2025-12-04 10:52:05 -0800

    update to standards from flip PR

--- torchvision/transforms/v2/_misc.py
+++ torchvision/transforms/v2/_misc.py
@@ -9,6 +9,7 @@ from torch.utils._pytree import tree_flatten, tree_unflatten
 
 from torchvision import transforms as _transforms, tv_tensors
 from torchvision.transforms.v2 import functional as F, Transform
+from torchvision.transforms.v2.functional._utils import _is_cvcuda_available, _is_cvcuda_tensor
 
 from ._utils import (
     _parse_labels_getter,
@@ -17,9 +18,11 @@ from ._utils import (
     get_bounding_boxes,
     get_keypoints,
     has_any,
-    is_cvcuda_tensor,
     is_pure_tensor,
 )
+
+
+CVCUDA_AVAILABLE = _is_cvcuda_available()
 
 
 # TODO: do we want/need to expose this?
@@ -268,7 +271,8 @@ class ToDtype(Transform):
             Default: ``False``.
     """
 
-    _transformed_types = (torch.Tensor, is_cvcuda_tensor)
+    if CVCUDA_AVAILABLE:
+        _transformed_types = Transform._transformed_types + (_is_cvcuda_tensor,)
 
     def __init__(
         self, dtype: Union[torch.dtype, dict[Union[type, str], Optional[torch.dtype]]], scale: bool = False
